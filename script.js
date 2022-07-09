ea39b653ee1614fde3515d8a89734f9cc9394e05
virginia-njeri <virginiaanjeri@gmail.com>
2022-07-09 14:16:07 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+  const panels = document.querySelectorAll('.panel')
+  panels.forEach((panel)=>{
+    addEventListener('click',()=>{
+        removeActiveClasses()
+        panel.classList.add('active')
+
+    })
+  })
+
+  function removeActiveClasses(){
+    panel.forEach(panels=>{
+
+        panels.classList.remove('active')
+
+    })
+  }
